b79e70ece8d99b8c977de5ed811745e780cae638
Mikk Tendermann <mikk150@gmail.com>
2015-10-23 18:20:22 +0300

    add authendication

--- HeaderParamAuth.php
+++ HeaderParamAuth.php
@@ -8,5 +8,26 @@ use yii\filters\auth\AuthMethod;
 */
 class HeaderParamAuth extends AuthMethod
 {
+    /**
+     * @var string the header parameter name for passing the access token
+     */
+    public $tokenParam = 'x-token';
 
+    /**
+     * @inheritdoc
+     */
+    public function authenticate($user, $request, $response)
+    {
+        $accessToken = $request->headers->get($this->tokenParam);
+        if (is_string($accessToken)) {
+            $identity = $user->loginByAccessToken($accessToken, get_class($this));
+            if ($identity !== null) {
+                return $identity;
+            }
+        }
+        if ($accessToken !== null) {
+            $this->handleFailure($response);
+        }
+        return null;
+    }
 }
